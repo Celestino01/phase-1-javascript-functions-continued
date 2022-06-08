fd5cf9dbbf19ff179f418d1f997124223e00f963
celestino <celestino.martinez01@gmail.com>
2022-06-08 16:29:48 -0400

    commit tree c610543fb3a5f4a987430fd5762109992eec71f6

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(activity){
+    const innerfunction = function(inner){
+        if(activity === '*'){
+            return `You are ${activity}${inner}${activity}!`;
+        }  
+        else if(activity === '||'){
+            return `You are ${activity}${inner}${activity}!`;
+        }
+    }
+
+    return innerfunction;
+}
+
